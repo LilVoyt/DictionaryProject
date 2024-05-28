310076a6ced1379d48034475da9808ddaef8c319
Voyt <olegvoyt6@gmail.com>
2024-05-28 13:12:36 +0300

    28.05Working

--- Dictionary/DictionaryList.cs
+++ Dictionary/DictionaryList.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,41 @@ namespace Dictionary
         public List<MyDictionary> Dictionaries { get; set; }
         public DictionaryList()
         {
-            Dictionaries = new List<MyDictionary>();
+            Dictionaries = ReadFromFile() ?? new List<MyDictionary>();
         }
 
         public void AddDictionary(string keyLanguage, string valueLanguage)
         {
+            ReadFromFile();
             Dictionaries.Add(new MyDictionary(keyLanguage, valueLanguage));
+            WriteToFile();
+        }
+
+        public void WriteToFile()
+        {
+            string json = JsonConvert.SerializeObject(Dictionaries);
+            using(StreamWriter sw = new StreamWriter("allDictionaries.txt", false))
+            {
+                sw.WriteLine(json);
+            }
+        }
+        public List<MyDictionary> ReadFromFile()
+        {
+            using(StreamReader sr = new StreamReader("allDictionaries.txt"))
+            {
+                Dictionaries = JsonConvert.DeserializeObject<List<MyDictionary>>(sr.ReadToEnd());
+            }
+            return Dictionaries;
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach(MyDictionary d in Dictionaries)
+            {
+                res += $"{d.KeyLanguage}-{d.ValueLanguage} Dictionary, ";
+            }
+            return res;
         }
     }
 }
--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -10,13 +10,25 @@ namespace Dictionary
     {
         static void Main()
         {
-            DictionaryList list = new DictionaryList();
-            list.AddDictionary("ua", "en");
-            list.Dictionaries[0].Add("word", new List<string> { "work", "adsf", "asdf" });
-            Console.WriteLine(list.Dictionaries[0]);
-            list.AddDictionary("en", "pl");
-            list.Dictionaries[0].Add("asgkhjty", new List<string> { "asdf", "3456v", "fmkhj" });
-            Console.WriteLine(list.Dictionaries[0]);
+            //DictionaryList list = new DictionaryList();
+            //list.AddDictionary("ua", "en");
+            //list.Dictionaries[0].Add("word", new List<string> { "work", "adsf", "asdf" });
+            //Console.WriteLine(list.Dictionaries[0]);
+            //list.AddDictionary("en", "pl");
+            //list.Dictionaries[0].Add("asgkhjty", new List<string> { "asdf", "3456v", "fmkhj" });
+            //Console.WriteLine(list.Dictionaries[0]);
+            //list.WriteToFile();
+
+            //DictionaryList list = new DictionaryList();
+            //list.ReadFromFile();
+            //Console.WriteLine(list.Dictionaries[0]);
+            ////list.Dictionaries[1].Add("word", new List<string> { "def", "def1" });
+            //Console.WriteLine(list.Dictionaries[1]);    
+            //list.WriteToFile();
+
+            DictionaryList dictionaryList = new DictionaryList();
+            dictionaryList.AddDictionary("ua", "pl");
+            Console.WriteLine(dictionaryList);
         }
     }
 }
