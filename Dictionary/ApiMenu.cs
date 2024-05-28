629d2234e1288f07ba862414b14478ca706c165d
Voyt <olegvoyt6@gmail.com>
2024-05-28 19:07:16 +0300

    28.05WorkingToPc

--- Dictionary/ApiMenu.cs
+++ Dictionary/ApiMenu.cs
@@ -32,6 +32,30 @@ namespace Dictionary
             if (int.TryParse(input, out choose) && choose >= 0 && choose < list.Dictionaries.Count)
             {
                 Console.WriteLine(list.Dictionaries[choose]);
+                Console.WriteLine("What you want to do");
+                Console.WriteLine("1 - add new word");
+                int toDo = int.Parse(Console.ReadLine());
+                switch (toDo)
+                {
+                    case 1:
+                        Console.Write($"Enter new word in {list.Dictionaries[choose].KeyLanguage}: ");
+                        string word = Console.ReadLine();
+                        Console.Write($"Enter the definitions in {list.Dictionaries[choose].ValueLanguage} (split by ', '): ");
+                        string value = Console.ReadLine();
+                        List<string> words = value.Split(new string[] { ", " }, StringSplitOptions.None).ToList();
+                        if (list.Dictionaries[choose].Pairs.ContainsKey(word))
+                        {
+                            list.Dictionaries[choose].Pairs[word].AddRange(words);
+                            list.WriteToFile();
+                        }
+                        else
+                        {
+                            list.Dictionaries[choose].Pairs.Add(word, words);
+                            list.WriteToFile();
+                        }
+                        Console.WriteLine(list.Dictionaries[choose].Pairs);
+                        break;
+                }
             }
             else
             {
@@ -46,6 +70,21 @@ namespace Dictionary
             {
                 Console.WriteLine($"[{count}] {d.KeyLanguage}-{d.ValueLanguage} Dictionary");
                 count++;
+            }
+        }
+
+        public void ShowDictByIndex(DictionaryList dictionaryList, int index)
+        {
+            if (index >= 0 && index < dictionaryList.Dictionaries.Count)
+            {
+                foreach (var pair in dictionaryList.Dictionaries[index].Pairs)
+                {
+                    Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid index.");
             }
         }
     }
