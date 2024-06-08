0888fc99ec10925b0e574060797ff4fb4e169eff
Voyt <olegvoyt6@gmail.com>
2024-06-08 23:30:46 +0300

    InitialCommit

---
+++ Quiz/Quiz/Data/QuizContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Data
+{
+    internal class QuizContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null;
+        public DbSet<Role> Roles { get; set; } = null;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ Quiz/Quiz/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Role { get; set; }
+        public virtual Role Roles { get; set; } = null;
+    }
+}
