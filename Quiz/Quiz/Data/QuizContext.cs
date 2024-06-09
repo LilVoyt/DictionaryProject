cd50974c479e7179d7d872fca3b371522b3c06f4
Voyt <olegvoyt6@gmail.com>
2024-06-09 15:20:54 +0300

    InitialCommit

--- Quiz/Quiz/Data/QuizContext.cs
+++ Quiz/Quiz/Data/QuizContext.cs
@@ -16,7 +16,7 @@ namespace Quiz.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Server=LAPTOP-U5SNHKPD\\SQLEXPRESS;Database=Quiz;Integrated Security=True;TrustServerCertificate=True");
         }
     }
 }
